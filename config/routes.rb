dd641165e7a31edf018ee7d1f96d7ceffa8920a4
susan <bluenote@outlook.com>
2018-12-23 14:09:14 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+
+  #resources :products, only: [:index, :add_to_cart]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+    root "products#index"
+    post "/" => "products#add"
+   end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
@@ -53,4 +57,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
